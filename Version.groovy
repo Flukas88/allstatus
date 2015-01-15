47675db4dda6e89f90b019fdc2c4639763f971ab
Luca Francesca <luca.francesca01@gmail.com>
2015-01-15 12:25:59 +0100

    Fixed a typo in http getting functions

--- Version.groovy
+++ Version.groovy
@@ -11,6 +11,7 @@ import java.util.regex.Pattern
 class AppVersion {
     public AppVersion() {
     } 
+
     /**
      * Get JMX data from host
      *
@@ -27,7 +28,8 @@ class AppVersion {
               def server        = JmxFactory.connect(new JmxUrl(serverUrl)).MBeanServerConnection
               def serverInfo    = new GroovyMBean(server, mbeanString).getProperty(versionAttributeName)
               return [serverHost, serverInfo]
-           } catch(Exception e) { }   
+           }
+           catch(Exception e) { }   
     } 
     /**
      * Get HTTP data from URI
@@ -37,8 +39,9 @@ class AppVersion {
      */ 
     private static getHttpVersions(URI) {
         try {
-            new URL(URI).getText()        
-        } catch (Exception e) { }
+            new URL(URI).getText()
+        }
+        catch (Exception e) { }
     }
     /**
      * Get data 
@@ -66,66 +69,71 @@ class AppVersion {
         def hosts = []
         def appList = []
         def tmpHost
-        new File("H:\\working\\config.txt").each { line ->
-            conf                      = line.split('#')
-            env                       = conf[0]
-            appName                   = conf[1]
-            clusterName               = conf[2]
-            appVersionProtocol        = conf[3]
-            appVersionURI             = conf[4]
-            platform1Name             = conf[5]
-            platform1Protocol         = conf[6]
-            platform1VersionURI       = conf[7]
-            platform2Name             = conf[8]
-            platform2Protocol         = conf[9]
-            platform2VersionURI       = conf[10]
-            hosts                     = conf[11].split(',') 
-            if (appName == AppName && env == appEnv) {
-                def conf2 
-                def tmp
-                def port        
-                def mbeansString 
-                def mbeansProp   
-                hosts.each { hostApp ->
-                    tmpHost = hostApp
-                    if (appVersionProtocol == "JMX") {
-                        conf2 = appVersionURI.split('@')
-                        port         = conf2[0]
-                        mbeansString = conf2[1]
-                        mbeansProp   = conf2[2]
-                        tmp =  getJmxVersion(hostApp, port, mbeansString, mbeansProp, AppName)
-                        tmpVersion = tmp[1]
-                        } 
-                    else {
-                        tmpVersion =  getHttpVersions(appVersionURI)
-                        }
-                    if (platform1Protocol == "JMX") {
-                        conf = platform1VersionURI.split('@')
-                        port         = conf[0]
-                        mbeansString = conf[1]
-                        mbeansProp   = conf[2]
-                        tmp =  getJmxVersion(hostApp, port, mbeansString, mbeansProp, AppName)
-                        platform1Version = tmp[1]
-                        } 
-                    else {
-                        tmpVersion =  getHttpVersions(platform1VersionURI)
-                        }
-                    if (platform2Protocol == "JMX") {
-                        conf = platform2VersionURI.split('@')
-                        port         = conf[0]
-                        mbeansString = conf[1]
-                        mbeansProp   = conf[2]
-                        tmp =  getJmxVersion(hostApp, port, mbeansString, mbeansProp, AppName)
-                        platform2Version = tmp[1]
-                        } 
-                    else {
-                        tmpVersion = getHttpVersions(platform2VersionURI)
-                        }
-                        appList.add([tmpHost, tmpVersion, platform1Name, platform1Version, platform2Name, platform2Version, clusterName])
-                }
-            } else { //discard the uneeded clutter 
-            }
-        }
+        def strReturn
+        try {
+          new File("/export/apps/appstatus/config.txt").each { line ->
+              conf                      = line.split('#')
+              env                       = conf[0]
+              appName                   = conf[1]
+              clusterName               = conf[2]
+              appVersionProtocol        = conf[3]
+              appVersionURI             = conf[4]
+              platform1Name             = conf[5]
+              platform1Protocol         = conf[6]
+              platform1VersionURI       = conf[7]
+              platform2Name             = conf[8]
+              platform2Protocol         = conf[9]
+              platform2VersionURI       = conf[10]
+              hosts                     = conf[11].split(',') 
+              if (appName == AppName && env == appEnv) {
+                  def conf2 
+                  def tmp
+                  def port        
+                  def mbeansString 
+                  def mbeansProp   
+                  hosts.each { hostApp ->
+                      tmpHost = hostApp
+                      if (appVersionProtocol == "JMX") {
+                          conf2 = appVersionURI.split('@')
+                          port         = conf2[0]
+                          mbeansString = conf2[1]
+                          mbeansProp   = conf2[2]
+                          tmp =  getJmxVersion(hostApp, port, mbeansString, mbeansProp, AppName)
+                          tmpVersion = tmp[1]
+                          } 
+                      else {
+                          tmpVersion =  getHttpVersions(appVersionURI)
+                          }
+                      if (platform1Protocol == "JMX") {
+                          conf = platform1VersionURI.split('@')
+                          port         = conf[0]
+                          mbeansString = conf[1]
+                          mbeansProp   = conf[2]
+                          tmp =  getJmxVersion(hostApp, port, mbeansString, mbeansProp, AppName)
+                          platform1Version = tmp[1]
+                          } 
+                      else {
+                          platform1Version =  getHttpVersions(platform1VersionURI)
+                          }
+                      if (platform2Protocol == "JMX") {
+                          conf = platform2VersionURI.split('@')
+                          port         = conf[0]
+                          mbeansString = conf[1]
+                          mbeansProp   = conf[2]
+                          tmp =  getJmxVersion(hostApp, port, mbeansString, mbeansProp, AppName)
+                          platform2Version = tmp[1]
+                          } 
+                      else {
+                          platform2Version = getHttpVersions(platform2VersionURI)
+                          }
+                          appList.add([tmpHost, tmpVersion, platform1Name, platform1Version, platform2Name, platform2Version, clusterName])
+                  }
+
+              } else { //discard the uneeded clutter 
+              }
+             
+          }
+          }catch(Exception e) {}
         
         def jsonBuilder = new groovy.json.JsonBuilder()
            jsonBuilder.app(
@@ -141,5 +149,7 @@ class AppVersion {
  
 }
 
+
 def ver = new me.lucafrancesca.gpack.AppVersion()
+
 println ver.getData(this.args[0], this.args[1])
